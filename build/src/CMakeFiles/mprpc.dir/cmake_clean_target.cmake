d7b07fc656f233d254353ce619321e67ca224264
GingerNan <ginger_nan@163.com>
2025-05-28 16:36:09 +0800

    first commit

---
+++ build/src/CMakeFiles/mprpc.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "/home/book/projects/mprpc/lib/libmprpc.a"
+)
---
+++ src/include/mprpcapplication.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "mprpcconfig.h"
+
+// mprpc框架的初始化类
+class MprpcApplication
+{
+public:
+    static void Init(int argc, char** argv);
+    static MprpcApplication& GetInstance();
+    static MprpcConfig& GetConfig();
+private:
+    MprpcApplication(){};
+    MprpcApplication(const MprpcApplication&) = delete;
+    MprpcApplication(MprpcApplication&&) = delete;
+
+private:
+    static MprpcConfig m_config;
+};
